63e9345425e678b9d8d9bf04a6fcb6f205db8de1
Rahul Rahul <rahul.mca.du.2015@gmail.com>
2021-10-05 10:37:15 +0200

    removed comment, changed variable name & type and changed addEventListener to onkeydown

--- src/controllers/Game.js
+++ src/controllers/Game.js
@@ -37,12 +37,11 @@ class Game {
   }
 
   getRandomTetromino() {
-    // TODO: add new classes for all tetromino shapes and create algorithm that will return tetromino of random type
     const pieces = [LShape, RLShape, LineShape, SquareShape, SShape, RSShape, TShape];
 
-    let r = Math.floor(Math.random() * 7); // 0 -> 6
+    const pieceIndex = Math.floor(Math.random() * 7); // 0 -> 6
 
-    return new pieces[r]();
+    return new pieces[pieceIndex]();
   }
 
   move(side) {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Game from './controllers/Game';
 const game = new Game();
 // game.main();
 setInterval(game.main, 500);
-document.addEventListener('keypress', (event) => {
+document.addEventListener('onkeydown', (event) => {
   if (event.key === '4') {
     game.move('left');
   }
